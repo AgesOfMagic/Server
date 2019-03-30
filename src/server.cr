b676f740f993b993645471daf713ba098c18cd16
theonlyartz <gamesil456@gmail.com>
2019-03-30 18:29:33 +0300

    Multiserver support

--- src/server.cr
+++ src/server.cr
@@ -1,47 +1,65 @@
 require "socket"
 require "./server/c_bindings/bindings.cr"
 require "./server/classes/*"
+require "colorize"
 
-clients_arr = Array(Player).new 0
+class Server
+  property port
+  property name
+  property clients_arr
+  getter! backend_helper : Backend
+  def initialize(@port : Int32, @name : String)
+    @clients_arr = [] of Player
+    @backend_helper = Backend.new(self)
 
-server = TCPServer.new(3000)
-loop do
-  if socket = server.accept?
-    # handle the client in a fiber
-    puts "Client connected"
-    spawn handle_connection(socket, clients_arr)
-  else
-    # another fiber closed the server
-    break
+    Logger.debug("Initializing a Server instance")
   end
-end
 
-def handle_connection(socket, clients_arr)
-  loop do
-    buffer = Bytes.new 512
-    socket.read buffer
-    # buffer = String.new buffer
-    decide_operation(buffer, socket, clients_arr)
+  def run
+    Logger.debug("Starting server at port #{@port}")
+    server = TCPServer.new(@port)
+    Logger.debug("A server is now running on port #{@port}")
+    loop do
+      if socket = server.accept?
+        # handle the client in a fiber
+        Logger.debug("A new client has been connected")
+        spawn handle_connection(socket)
+      else
+        # another fiber closed the server
+        break
+      end
+    end
   end
-rescue ex : Errno
-  if ex.errno == Errno::ECONNRESET || ex.errno == Errno::EPIPE
-    if socket_to_disconnect = clients_arr.find {|o| o.tcp == socket}
-      clients_arr.delete(socket_to_disconnect)
-      puts "Disconnected succesfully"
-      puts "LENGTH AFTER DISCONNECT => #{clients_arr.size}"
+
+  def handle_connection(socket)
+    loop do
+      buffer = Bytes.new 512
+      socket.read buffer
+      Logger.debug("Reading a new buffer from a socket")
+      # buffer = String.new buffer
+      decide_operation(buffer, socket)
+    end
+  rescue ex : Errno
+    Logger.error(ex)
+    if socket_to_disconnect = @clients_arr.find {|o| o.tcp == socket}
+      @clients_arr.delete(socket_to_disconnect)
+      Logger.debug("A socket has been disconnected succesfully")
+    end
+  end
+
+  def decide_operation(message, client)
+    id = String.new(message)[0] # The first byte is in charge of the stage
+    curr_type = Protocol.identify(id.ord.to_u8, 0)
+
+    case curr_type
+    when Protocol::PacketTypes::HAND_SHAKE_CLIENT_TYPE
+      Logger.debug("Establishing an handshake client type")
+      backend_helper.handle_handshake_client_type(message, client)
+    when Protocol::PacketTypes::MOVEMENT_PACKET_TYPE
+      backend_helper.handle_movement_packet_type(message, client)
     end
   end
 end
 
-def decide_operation(message, client, clients_arr)
-  id = String.new(message)[0] # The first byte is in charge of the stage
-  curr_type = Protocol.identify(id.ord.to_u8, 0)
-  
-  case curr_type
-  when Protocol::PacketTypes::HAND_SHAKE_CLIENT_TYPE
-    puts "Establishing an handshake client type"
-    Backend::Helper.handle_handshake_client_type(message, client, clients_arr)
-  when Protocol::PacketTypes::MOVEMENT_PACKET_TYPE
-    Backend::Helper.handle_movement_packet_type(message, client, clients_arr)
-  end
-end
+new_server = Server.new 3000, "Amitx64_Crystal"
+new_server.run
--- src/server/classes/backend_helper.cr
+++ src/server/classes/backend_helper.cr
@@ -1,28 +1,32 @@
 # This class is being in charge of the handling of incoming packets
-class Backend::Helper
+class Backend
+  def initialize(server : Server)
+    @server = server
+  end
+
   # Handles the HANDLE_HANDSHAKE_CLIENT_TYPE packets
   # the code will generate a raw_client
-  def self.handle_handshake_client_type(message, client, clients_arr)
+  def handle_handshake_client_type(message, client)
     # Extract the raw client from the buffer
     raw_client = Protocol.bufferToHandShakeClient(message)
     # Construct a new player from the raw_client : Player
     constructed_client = Player.construct(client, raw_client)
     # Push the new client to the clients array
-    clients_arr << constructed_client
+    @server.clients_arr << constructed_client
 
 
     # Construct a new HandShake server
     hand_shake_struct = Protocol::HandShakeServer.new(
       status: Protocol::Status::OK,
       serverVersion: StaticArray(UInt8, 16).new(0_u8),
-      characterData: StaticArray(UInt8, 492).new(0_u8))
+      serverName: StaticArray(UInt8, 128).new {|i| @server.name.byte_at?(i) || 0_u8})
     buff = Protocol.handShakeServerToBuffer(hand_shake_struct)
 
     # Send the new buffer
     client.send(buff.to_slice(512))
 
     # Send the already connected player's position
-    clients_arr.each do |player|
+    @server.clients_arr.each do |player|
 
       updatePositionStruct = Protocol::UpdatePosition.new(
       x: player.@posX,
@@ -31,16 +35,15 @@ class Backend::Helper
 
       # Convert the UpdatePosition packet to sendable buffer
       updatePosBuff = Protocol.updatePositionToBuffer(updatePositionStruct)
-      print(updatePosBuff)
       client.send(updatePosBuff.to_slice(512))
     end
   end
 
   # Handles the HANDLE_MOVEMENT_PACKET_TYPE packets
-  def self.handle_movement_packet_type(message, client, clients_arr)
+  def handle_movement_packet_type(message, client)
     movement_packet = Protocol.bufferToMovement(message)
     # Broadcast the new position with UpdateMovement
-    if desired_player = clients_arr.find {|o| o.tcp == client}
+    if desired_player = @server.clients_arr.find {|o| o.tcp == client}
       desired_player.update_position(movement_packet.direction)
       updatePositionStruct = Protocol::UpdatePosition.new(
       x: desired_player.@posX,
@@ -48,15 +51,15 @@ class Backend::Helper
       id: desired_player.@id)
 
       buffer = Protocol.updatePositionToBuffer(updatePositionStruct)
-      clients_arr.each do |socket|
-        puts "
-        Sending new update position buffer to #{socket.@id}
-        ====================#{movement_packet.direction}=========================
-        Client array count =======> #{clients_arr.size}
-        ===============================================
-        Position X: #{desired_player.@posX}
-        Position Y: #{desired_player.@posY}
-        "
+      @server.clients_arr.each do |socket|
+        # puts "
+        # Sending new update position buffer to #{socket.@id}
+        # ====================#{movement_packet.direction}=========================
+        # Client array count =======> #{clients_arr.size}
+        # ===============================================
+        # Position X: #{desired_player.@posX}
+        # Position Y: #{desired_player.@posY}
+        # "
         socket.tcp.send(buffer.to_slice(512))
       end
     end
